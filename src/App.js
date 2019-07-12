1866bafadc8699a7aaada0ae39b68d0ca3ee799e
Alex <alex@alexs-mbp.home>
2019-07-12 18:24:58 +0100

    destructuring props

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// Destructuring
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "something important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Elmo",
+      animal: "Cat",
+      breed: "Ragdoll"
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
